90a475d99b894d5c8946a23264b002f71a43048a
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-16 15:02:37 +0200

    emulate popup on page load

--- src/comments.js
+++ src/comments.js
@@ -1,3 +1,4 @@
+import closeIcon from './img/close_icon.png';
 import Config from './config';
 
 const config = new Config();
@@ -5,7 +6,6 @@ export default class CommentsPopUp {
   constructor() {
     // for testing purposes
     this.popUp = document.createElement('section');
-    console.log(this.popUp);
     this.popUp.setAttribute('id', 'commentsPopUp');
     document.querySelector('body').innerHTML += '<span class="btn-comments" data="123">comments</span>';
   }
@@ -14,31 +14,32 @@ export default class CommentsPopUp {
     if (id) {
       this.getProductInfo(id).then((productInfo) => {
         if (productInfo.error === false) {
-          const { title, image } = productInfo.data;
-          const closeIcon = `<svg xmlns='http://www.w3.org/2000/svg' x='0px' y='0px'
-          width='50' height='50' viewBox='0 0 172 172' style=' fill:#000000;'><g fill='none' fill-rule='nonzero' stroke='none' stroke-width='1' stroke-linecap='butt' stroke-linejoin='miter' stroke-miterlimit='10' stroke-dasharray=' stroke-dashoffset='0' font-family='none' font-weight='none' font-size='none' text-anchor='none' style='mix-blend-mode: normal'><path d='M0,172v-172h172v172z' fill='none'></path><g fill='#000000'><path d='M107.33488,86l46.8872,-70.3308c0.70176,-1.05608 0.77056,-2.41144 0.172,-3.52944c-0.59856,-1.118 -1.76472,-1.81976 -3.03408,-1.81976h-25.2496c-1.12488,0 -2.18096,0.5504 -2.82424,1.47576l-37.28616,53.56424l-37.2896,-53.56424c-0.64328,-0.92536 -1.69592,-1.47576 -2.8208,-1.47576h-25.2496c-1.26936,0 -2.43552,0.69832 -3.03408,1.81632c-0.59856,1.118 -0.52976,2.4768 0.172,3.52944l46.8872,70.33424l-46.8872,70.3308c-0.70176,1.05608 -0.77056,2.41144 -0.172,3.52944c0.59856,1.118 1.76472,1.81976 3.03408,1.81976h25.2496c1.12488,0 2.18096,-0.5504 2.82424,-1.47576l37.28616,-53.56424l37.2896,53.56424c0.64328,0.92536 1.69592,1.47576 2.8208,1.47576h25.2496c1.26936,0 2.43552,-0.69832 3.03408,-1.81632c0.59856,-1.118 0.52976,-2.4768 -0.172,-3.52944z'></path></g></g></svg>`;
+          const { title, image, category, price, description } = productInfo.data;
           this.popUp.style.display = 'flex';
+          this.popUp.innerHTML = 'Loading...';
           this.popUp.innerHTML = `
-            <img src="${closeIcon}" class="close-comment-popup">
+            <img src="${closeIcon}" title="close" class="close-comment-popup">
             <img src="${image}" class="comment-cover">
             <h2>${title}</h2>
             <p>
-              <ul>
+              <ul class="d-flex">
                 <li>
-                Price: 
+                <b>Price:</b> ${price}
+                </li>
+                <li>
+                  <b>Category:</b> ${category}
                 </li>
               </ul>
-              
+                <em>
+                  ${description}
+                </em>
             </p>
           `;
-          console.log(this.popUp);
-          document.querySelector('body').innerHTML += this.popUp;
-        } else {
-          console.log(productInfo);
+          document.querySelector('body').appendChild(this.popUp);
+          const closeBtn = document.querySelector('.close-comment-popup');
+          closeBtn.addEventListener('click', () => this.hide());
         }
       });
-    } else {
-      console.log('id is missing');
     }
   }
 
