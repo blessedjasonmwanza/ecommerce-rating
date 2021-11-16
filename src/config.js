952ed7d91d0c36ca247a325b09ba4b081a1d68db
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-16 14:01:35 +0200

    Add config.js and comments.js for popup

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default class Config {
+  constructor() {
+    this.involvementAppId = 'dwHwCqYFHfSrZFnmOxOx';
+    this.involvementBaseUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/${this.involvementAppId}/`;
+    this.likesEndPoint = `${this.involvementBaseUrl}/likes`;
+    this.commentsEndPoint = `${this.involvementBaseUrl}/comments`;
+    this.productsBaseUrl = 'https://fakestoreapi.com/products';
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './style.css';
+import CommentsPopUp from './comments';
+
+const comments = new CommentsPopUp();
+comments.display('1');
