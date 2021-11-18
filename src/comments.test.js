d558b95233627c22936fa6c3db26599e77411785
blessedjasonmwanza <mwanzabj@gmail.com>
2021-11-18 09:19:50 +0200

    Add another test counter for comments

--- src/comments.test.js
+++ src/comments.test.js
@@ -6,10 +6,15 @@ import CommentsPopUp from './comments.js';
 jest.setTimeout(10000);
 const comments = new CommentsPopUp();
 describe('Count comments', () => {
-  test('Add new comment and expect total to exceed initial comments', async () => {
+  test('Add new comment and expect total comments to exceed initial comments count', async () => {
     const resulta = await comments.totalComments(2);
     await comments.add('2', 'testuser', 'jest');
     const resultb = await comments.totalComments(2);
     expect(resultb).toBeGreaterThan(resulta);
+  });
+  test('Expect total comments not to change when no new item is added', async () => {
+    const resulta = await comments.totalComments(2);
+    const resultb = await comments.totalComments(2);
+    expect(resultb).toEqual(resulta);
   });
 });
